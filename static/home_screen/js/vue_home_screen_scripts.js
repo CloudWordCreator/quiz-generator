3f4fdda3565923fc03edb4973109270e87e121e8
wsdnakamoto <wsdnakamoto@gmail.com>
2025-02-25 10:20:41 +0900

    feat: home, login, reportの画面へ遷移するためのボタンをまとめたspeeddialを作成

--- static/home_screen/js/vue_home_screen_scripts.js
+++ static/home_screen/js/vue_home_screen_scripts.js
@@ -1,5 +1,9 @@
 const { createApp } = Vue;
+
 const app1 = createApp({
+    // setup() {
+    //     document.querySelector(".hideBeforeVue").classList.remove("hideBeforeVue");
+    // },
     data() {
         return {
             sample: "a"
@@ -7,7 +11,7 @@ const app1 = createApp({
     },
     compilerOptions: {
         delimiters: ['[[', ']]'],
-    }
+    },
 });
 
 app1.use(PrimeVue.Config, {
@@ -17,3 +21,41 @@ app1.use(PrimeVue.Config, {
 });
 app1.component('p-datepicker', PrimeVue.DatePicker);
 app1.mount('#app1');
+
+const speeddial = createApp({
+    data() {
+        return {
+            items: [
+                {
+                    label: 'home',
+                    icon: 'pi pi-home',
+                    command: () => {
+                        window.location.href = '/home';
+                    }
+                },
+                {
+                    label: 'login',
+                    icon: 'pi pi-sign-in',
+                    command: () => {
+                        window.location.href = '/login';
+                    }
+                },
+                {
+                    label: 'report',
+                    icon: 'pi pi-envelope',
+                    command: () => {
+                        window.location.href = 'report';
+                    }
+                },
+            ]
+        }
+    },
+});
+
+speeddial.use(PrimeVue.Config, {
+    theme: {
+        preset: PrimeVue.Themes.Aura,
+    },
+});
+speeddial.component('p-speeddial', PrimeVue.SpeedDial);
+speeddial.mount('#speeddial');
